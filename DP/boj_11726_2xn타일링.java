44b95a8c56503bb876f1a881ee28fda27168e3d2
hhytruth <hhytruth@gmail.com>
2021-03-24 10:23:49 +0900

    Create boj_11726_2xn타일링.java
    https://www.acmicpc.net/problem/11726

---
+++ DP/boj_11726_2xn타일링.java
@@ -0,0 +1,29 @@
+package swing;
+import java.io.*;
+import java.util.*;
+
+
+public class boj_11726_2xn타일링 {
+	
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		int[][] d=new int[n][2];
+		
+		d[0][0]=1;
+		d[0][1]=0;
+		if(n>1) {
+			d[1][0]=1;
+			d[1][1]=1;	
+		}
+		
+		
+		for(int i=2;i<n;i++) {
+			d[i][0]=(d[i-1][0]+d[i-1][1])%10007;
+			d[i][1]=(d[i-2][0]+d[i-2][1])%10007;
+		}
+			
+		System.out.println((d[n-1][0]+d[n-1][1])%10007);
+	}
+}
