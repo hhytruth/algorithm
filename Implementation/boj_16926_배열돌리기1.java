12cdf15f85fdf20a24e2ecc8c855f13335ceb727
hhytruth <hhytruth@gmail.com>
2021-03-26 09:33:09 +0900

    Update and rename Others/boj_16926_배열 돌리기1.java to Implementation/boj_16926_배열돌리기1.java
    https://www.acmicpc.net/problem/16926

--- Implementation/boj_16926_배열돌리기1.java
+++ Implementation/boj_16926_배열돌리기1.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Array {
+public class boj_16926_배열돌리기 {
 	
 	public static void main(String[] args) throws Exception {
 		
