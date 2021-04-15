b4e31c62ca17501233f4c080636a565d28ecc432
hhytruth <hhytruth@gmail.com>
2021-04-15 13:05:05 +0900

    Create boj_10844_쉬운계단수.java
    https://www.acmicpc.net/problem/10844

---
+++ DP/boj_10844_쉬운계단수.java
@@ -0,0 +1,31 @@
+package com.thread;
+import java.util.*;
+import java.io.*;
+
+public class boj_10844_쉬운계단수 {
+
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		int[][] dp=new int[N+1][10];
+		
+		for(int i=1;i<10;i++) {
+			dp[1][i]=1;
+		}
+		for(int i=2;i<=N;i++) {
+			for(int j=0;j<10;j++) {
+				if(j==0)dp[i][j]=dp[i-1][j+1]%1000000000;
+				else if(j==9)dp[i][j]=dp[i-1][j-1]%1000000000;
+				else dp[i][j]=(dp[i-1][j-1]+dp[i-1][j+1])%1000000000;
+			}
+		}
+		
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			sum=(sum+dp[N][i])%1000000000;
+		}
+		System.out.println(sum);
+	}
+
+}
