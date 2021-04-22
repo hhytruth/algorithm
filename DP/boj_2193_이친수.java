cff901c272077236216912fa8aea14d55d031ed1
hhytruth <hhytruth@gmail.com>
2021-04-22 16:52:43 +0900

    Create boj_2193_이친수.java
    https://www.acmicpc.net/problem/2193

---
+++ DP/boj_2193_이친수.java
@@ -0,0 +1,30 @@
+package algorithm.dp;
+import java.io.*;
+import java.util.*;
+
+public class boj_2193_이친수 {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		long[][] dp=new long[N+1][2];
+		dp[1][0]=0;
+		dp[1][1]=1;
+		if(N==1) {
+			System.out.println(1);
+			return;
+		}
+		
+		dp[2][0]=1;
+		dp[2][1]=0;
+		
+		for(int i=3;i<=N;i++) {
+			dp[i][0]=dp[i-1][0]+dp[i-1][1];
+			dp[i][1]=dp[i-1][0];
+		}
+		
+		System.out.println(dp[N][0]+dp[N][1]);
+	}
+
+}
