e5b3327ec155762392616b7e01370ab68f4c5714
hhytruth <hhytruth@gmail.com>
2021-09-21 03:17:56 +0900

    Create programmers_주식가격.java
    https://programmers.co.kr/learn/courses/30/lessons/42584

---
+++ Stack&Queue/programmers_주식가격.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class programmers_주식가격 {
+    public int[] solution(int[] prices) {        
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+        }
+        return answer;
+    }
+}
